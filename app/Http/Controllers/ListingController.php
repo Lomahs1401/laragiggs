2ce3b1156324fa3ff31b28c6b5a00619a24f456c
lehoanglong12345 <lelong.ll32@gmail.com>
2023-01-16 23:14:36 +0700

    First commit

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    // Show all listings
+    public function index() {
+        return view('listings.index', [
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->get()
+        ]);
+    }
+
+    // Show single listing
+    public function show($id) { 
+        $listing = Listing::find($id);
+
+        if ($listing) {
+            return view('listings.show', [
+                'listing' => $listing
+            ]);
+        } else {
+            abort('404');
+        }
+    }
+}
